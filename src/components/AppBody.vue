2b21f6e78bb0074ec6a8c38b18cde7869d45115e
Marcelo Rubens Raulino <mraulino@monorail.com>
2022-10-19 15:59:41 -0300

    added pwa

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -8,9 +8,9 @@
     <div class="map-block m-b-40">
       <img src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/pass/GoogleMapTA.jpg" alt="">
       <img src="../assets/img/pinLogo.svg" class="pin">
-      <button class="button">Abrir mapa</button>
+      <a href="https://goo.gl/maps/gc2tiL7DSWxRqEf1A" target="_blank" class="d-block button">Abrir mapa</a>
     </div>
-    <p class="text-white m-0">Links úteis do pico:</p>
+    <p class="text-white m-0">Links úteis do pico</p>
     <ul class="custom-list">
       <li><a href="3">Instagram</a></li>
       <li><a href="3">Orkut</a></li>
