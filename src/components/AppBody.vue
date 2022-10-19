42a2ed0d3964eb891bb1cf61bacc78efb7266662
Marcelo Rubens Raulino <mraulino@monorail.com>
2022-10-19 12:13:55 -0300

    first commit

---
+++ src/components/AppBody.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex flex-column align-items-start">
+    <p>Ela tá em</p>
+    <h1 class="text-left text-white m-0 m-b-20">
+      La Cave Bistrô Boi & Lanches
+    </h1>
+    <p>Rua Xablau 69420</p>
+    <div class="map-block m-b-40">
+      <img src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/master/pass/GoogleMapTA.jpg" alt="">
+      <img src="../assets/img/pinLogo.svg" class="pin">
+      <button class="button">Abrir mapa</button>
+    </div>
+    <p class="text-white m-0">Links úteis do pico:</p>
+    <ul class="custom-list">
+      <li><a href="3">Instagram</a></li>
+      <li><a href="3">Orkut</a></li>
+      <li><a href="3">LinkedIn</a></li>
+    </ul>
+  </div>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="d-flex align-items-center m-b-20">
+    <img src="../assets/img/pinLogo.svg" class="m-r-20">
+    <h1 class="text-left">
+      Onde tá a Snurf?
+    </h1>
+  </div>
+</template>
